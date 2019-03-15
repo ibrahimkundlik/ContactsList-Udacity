ec16cfb01f098de301068c149abc9d153f51250b
ibrahimkundlik <ibrahimkundlik@gmail.com>
2019-03-15 11:45:39 +0530

    Updates

---
+++ precache-manifest.80bdb15b653a454e0d536f70352eea09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contacts-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "86e8db04a64ff82e07f3",
+    "url": "/contacts-list/static/js/runtime~main.9d1d4cd3.js"
+  },
+  {
+    "revision": "ed85217f78d98920ea24",
+    "url": "/contacts-list/static/js/main.87d7720a.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/contacts-list/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "ed85217f78d98920ea24",
+    "url": "/contacts-list/static/css/main.653158a8.chunk.css"
+  },
+  {
+    "revision": "e750cf70966d5a7d3c0f61c1f5b2458b",
+    "url": "/contacts-list/index.html"
+  }
+];
